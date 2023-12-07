272a9bce6c83958bd1ce76203199930aa3daeebd
sergii-sami <sergii.mishchaniuk@samiedu365.fi>
2023-12-07 10:33:20 +0200

    Change copyTextClipboard function

--- 9_uusi_css_ominaisuus/script.js
+++ 9_uusi_css_ominaisuus/script.js
@@ -25,17 +25,9 @@ function showHideRegions (name) {
     };
 }
 function copyTextClipboard(text_field_id) {
-    // Get the text field
-    var copyText = document.getElementById(text_field_id);
-    alert(copyText);
+    // Get the code text
+    var copyText = document.getElementById(text_field_id).innerHTML;
   
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-    
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  }
+    // Copy to clipboard
+    navigator.clipboard.writeText(copyText);
+}
