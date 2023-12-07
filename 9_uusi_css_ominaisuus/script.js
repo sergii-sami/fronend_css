8eed62e484c5a9b47bd2494c643fba0fa6efdf5f
sergii-sami <sergii.mishchaniuk@samiedu365.fi>
2023-12-07 11:01:10 +0200

    showDialog function

--- 9_uusi_css_ominaisuus/script.js
+++ 9_uusi_css_ominaisuus/script.js
@@ -3,7 +3,7 @@ function hideOnLoad () {
     const code = document.getElementById('code');
     img.hidden = true;
     code.hidden = true;
-}
+};
 function showHideRegions (name) {
     const pres = document.getElementById('article');
     const img = document.getElementById('figure');
@@ -23,11 +23,22 @@ function showHideRegions (name) {
             code.hidden = true;
         };
     };
-}
-function copyTextClipboard(text_field_id) {
+};
+function copyTextClipboard(text_field_id, code_dialog_id) {
     // Get the code text
     var copyText = document.getElementById(text_field_id).innerHTML;
   
     // Copy to clipboard
     navigator.clipboard.writeText(copyText);
+
+    // Run showDialog function
+    showDialog(code_dialog_id);
 }
+function showDialog (code_dialog_id) {
+    var dialog = document.getElementById(code_dialog_id);
+    if (dialog.open == true) {
+        dialog.close();
+    } else {
+        dialog.show();
+    };
+};
