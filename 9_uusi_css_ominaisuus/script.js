7df081bcf9085944a5e9aaba4129fd4c100dce80
sergii-sami <sergii.mishchaniuk@samiedu365.fi>
2023-12-07 10:23:50 +0200

    copyTextClipboard function

--- 9_uusi_css_ominaisuus/script.js
+++ 9_uusi_css_ominaisuus/script.js
@@ -24,3 +24,18 @@ function showHideRegions (name) {
         };
     };
 }
+function copyTextClipboard(text_field_id) {
+    // Get the text field
+    var copyText = document.getElementById(text_field_id);
+    alert(copyText);
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
